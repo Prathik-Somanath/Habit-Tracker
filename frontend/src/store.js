d5ab6c1e7eb98bde39c9e8305794c0f484578f74
Robowizx <omkariyer99@gmail.com>
2020-12-18 22:20:35 +0530

    added comments

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,8 +4,11 @@ import React, { createContext, useReducer } from 'react';
 const store = createContext(null);
 const { Provider } = store;
 
+//provider component for global context
 const StateProvider = ({ children }) => {
+  //reducer that is stored in the global context
   const [state, dispatch] = useReducer(
+    //Reducer function that mimics setState function and updates proerties upto single level depth
     (prevState, action) => {
       if (action) {
         const newState = { ...prevState };
@@ -21,7 +24,7 @@ const StateProvider = ({ children }) => {
       prevDownload: null,
     },
   );
-
+  //initializing context with a reducer state and dispatch functions 
   return <Provider value={{ state, dispatch }}>{children}</Provider>;
 };
 
